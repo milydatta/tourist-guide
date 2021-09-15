f299863504ddcee38ee434d837b171b9b1c30da4
mily datta <dmily270@gmail.com>
2021-09-15 22:19:46 +0600

    some update

--- src/components/Home/FooterCol/FooterCol.js
+++ src/components/Home/FooterCol/FooterCol.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const FooterCol = (props) => {
     return (
-        <div className="col-md-6" style={{paddingLeft:200,textDecoration:"none"}}>
+        <div className="col-md-6" style={{paddingLeft:200}}>
             <h6 className="text-success">{props.menuTitle ? props.menuTitle : " "}</h6>
             <ul className="list-unstyled mt-4">
                   {
-                      props.menuItems.map((item, index) => <li key={index}><Link to={item.link} className="text-secondary">{item.name}</Link></li>)
+                      props.menuItems.map((item, index) => <li key={index}><Link to={item.link} className="text-secondary"  style={{textDecoration: "none"}}>{item.name}</Link></li>)
                   }
              </ul>
              {props.children && props.children}
@@ -16,22 +16,3 @@ const FooterCol = (props) => {
 };
 
 export default FooterCol;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const FooterCol = (props) => {
-//     return (
-//         <div className="col-md-3">
-//             <h6 className="text-primary">{props.menuTitle ? props.menuTitle : " "}</h6>
-//             <ul className="list-unstyled mt-4">
-//                  {
-//                      props.menuItems.map((item, index) => <li key={index}><Link to={item.link} className="text-secondary">{item.name}</Link></li>)
-//                  }
-//             </ul>
-//             {props.children && props.children}
-//         </div>
-//     );
-// };
-
-// export default FooterCol;
--- src/components/Shared/Sidebar/Sidebar.js
+++ src/components/Shared/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faCalendar, faGripHorizontal, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faCalendar, faGripHorizontal, faUsers, faHome } from '@fortawesome/free-solid-svg-icons';
 
 
 const Sidebar = () => {
@@ -11,23 +11,28 @@ const Sidebar = () => {
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{height:"100vh"}}>
             <ul className="list-unstyled">
                 <li>
-                    <Link to="/dashboard" className="text-white">
+                    <Link to="/dashboard" className="text-white item">
                         <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span> 
                     </Link>
                 </li>
                 <li>
-                    <Link to="/bookings" className="text-white">
+                    <Link to="/bookings" className="text-white item">
                         <FontAwesomeIcon icon={faCalendar} /> <span>Bookings</span> 
                     </Link>
                 </li>
                 <li>
-                    <Link to="/consultings" className="text-white">
+                    <Link to="/consultings" className="text-white item">
                         <FontAwesomeIcon icon={faUsers} /> <span>Consulting</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/setting" className="text-white" >
+                    <Link to="/setting" className="text-white item" >
                       <FontAwesomeIcon icon={faCog} /> <span>Setting</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/#" className="text-white item" >
+                      <FontAwesomeIcon icon={faHome} /> <span>Home</span>
                     </Link>
                 </li>
             </ul>
